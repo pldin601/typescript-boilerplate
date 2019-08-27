241b8ee881e2aca8634a10c389746c5f8009b7b3
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-08-27 21:06:09 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}", "!src/**/*.d.ts"]
